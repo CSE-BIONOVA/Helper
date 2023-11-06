694d6a39dc7b6d14fa1a26f10cffd7bfb6f384f5
csebionova <cse.bionova@gmail.com>
2023-11-06 08:39:52 +0000

    script changes

--- genome_download.sh
+++ genome_download.sh
@@ -1,9 +1,13 @@
 # species type
 SPECIES=$1
 FILENAME=$2
-ASSEMBLY_SUMMARY= "../Genome/$SPECIES/assembly_summary.txt"
+ASSEMBLY_SUMMARY="../../Genome/$SPECIES/assembly_summary.txt"
 while IFS= read -r line 
 do 
-	LINK=$(grep $line $ASSEMBLY_SUMMARY | cut -f20) 
+	#LINK=$(grep $line $ASSEMBLY_SUMMARY | cut -f20) 
+	#SPECIES_NAME=$(grep $line $ASSEMBLY_SUMMARY | cut -f7)
+	LINK= cut -f20
+	SPECIES= cut -f7
+	echo $SPECIES
 	wget $LINK
-done < $FILENAME
+done < $ASSEMBLY_SUMMARY
