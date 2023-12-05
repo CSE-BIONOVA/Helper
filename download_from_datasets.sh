8cae7a5360dfc28c7a96bce18c967aeca92b24be
Samadhi Kariyawasam <samadhi.19@cse.mrt.ac.lk>
2023-12-05 23:02:06 +0530

    added download virus code

---
+++ download_from_datasets.sh
@@ -0,0 +1,16 @@
+
+file="/accession/virus_accession.txt"
+count=0
+
+while IFS= read -r line
+do
+  echo "Processing line: $line"
+  datasets download virus genome accession $line
+  unzip ncbi_dataset.zip
+  touch $line.fna
+  mv ncbi_dataset/data/genomic.fna $line.fna
+  count=$((count+1))
+  if [ $count -eq 3000 ]; then
+    break
+  fi
+done < "$file"
