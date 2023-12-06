6c2511e61ba7276c7fa424effcfc6bba16f62aa9
Samadhi Kariyawasam <samadhi.19@cse.mrt.ac.lk>
2023-12-06 18:43:32 +0530

    added bactria downloader

--- download_bacteria_ftp.py
+++ download_bacteria_ftp.py
@@ -1,7 +1,7 @@
 import csv
 import urllib.request
 
-csv_file = 'prokaryotes.csv'
+csv_file = '../../BashScripts/Helper/bacteria_human.csv'
 new_csv_file = 'virus_accession.csv'
 
 with open(csv_file, 'r') as file:
