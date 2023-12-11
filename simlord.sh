fa44ba5de09331f6dd298b782a980f2de837a2e0
Samadhi Kariyawasam <samadhi.19@cse.mrt.ac.lk>
2023-12-11 23:14:13 +0530

    added coverage calculation for simlord

--- simlord.sh
+++ simlord.sh
@@ -6,14 +6,7 @@ do
     file_path=${line[0]}
     coverage=${line[1]}
     file_name=${line2[0]}
-
-    if ($file_name == "GRCh38"); then
-        continue
-    fi
-
     simlord --read-reference $file_path --coverage $coverage --no-sam $file_name 
 
 done 3< "$coverage_list" 4< "$abundance_list"
 
-simlord --read-reference "../../Genome/Human/ncbi_dataset/data/GCF_000001405.40/GCF_000001405.40_GRCh38.p14_genomic.fna" --coverage 1466 --no-sam "GRCh38"
-       
