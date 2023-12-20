7c2aa211050dfea561088173ddd148b52c8522de
csebionova <cse.bionova@gmail.com>
2023-12-20 11:00:36 +0530

    added fasta file

--- file_size.sh
+++ file_size.sh
@@ -3,6 +3,10 @@ abundance_list='../../BashScripts/Helper/accession/human_metagenome/temp_files/a
 # genome_list='accession/human_metagenome/temp_files/dna_list.csv'
 # abundance_list='accession/human_metagenome/temp_files/abundance.csv'
 coverage_list='coverage_list.csv'
+quantity_list='quantity_list.csv'
+
+> $coverage_list
+> $quantity_list
 
 full_size=0
 while IFS=',' read -r -a line
@@ -19,9 +23,10 @@ do
     file_path=${line[1]}
     real_file_size=$(stat -c%s "$file_path")
     abundance=${line2[1]}
-    simlated_read_size=$(echo "$full_size * $abundance" | bc)
-    coverage=$(echo "$simlated_read_size / $real_file_size" | bc)
-    echo "$file_path,$simulated_read_size" >> "$coverage_list"
+    simulated_read_size=$(echo "$full_size * $abundance" | bc)
+    coverage=$(echo "$simulated_read_size / $real_file_size" | bc)
+    echo "$file_path,$coverage" >> "$coverage_list"
+    echo "$file_path,$simulated_read_size" >> "$quantity_list"
 
 done 3< "$genome_list" 4< "$abundance_list"
        
