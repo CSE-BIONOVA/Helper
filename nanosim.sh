c1f16c379423e3750daab46977e2b0c6f161f1cb
Samadhi Kariyawasam <samadhi.19@cse.mrt.ac.lk>
2023-12-22 16:38:33 +0530

    added labels for the final fasta file

--- nanosim.sh
+++ nanosim.sh
@@ -1,4 +1,5 @@
-referance='/home/mbadmin/Documents/BashScripts/Helper/accession/human_metagenome/no_of_reads.csv'
+referance='/home/mbadmin/Documents/BashScripts/Helper/accession/human_metagenome/bacteria_files.csv'
+filenames='/home/mbadmin/Documents/BashScripts/Helper/accession/human_metagenome/reads/bacteria_reads.csv'
 
 while IFS=',' read -r -a line
 do 
@@ -6,6 +7,8 @@ do
     file_name=${line[0]}
     dna_type=${line[2]}
     no_of_reads=${line[3]}
-    ../../Simtools/NanoSim/src/simulator.py genome -rg $file_path -c ../../Simtools/NanoSim/pre-trained_models/metagenome_ERR3152364_Even/training -dna_type $dna_type -o $file_name  -n  $no_of_reads 
+    x = ../../Simtools/NanoSim/src/simulator.py genome -rg $file_path -c ../../Simtools/NanoSim/pre-trained_models/metagenome_ERR3152364_Even/training -dna_type $dna_type -o human/reads/$file_name  -n  $no_of_reads 
+    printf "------------------"
+    printf "$x\n"
     
 done < "$referance"
