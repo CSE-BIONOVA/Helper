6ed4bd261b01b1ded4a1ada23d3b645c10b31c6d
Samadhi Kariyawasam <samadhi.19@cse.mrt.ac.lk>
2023-12-22 23:02:40 +0530

    added labels for the final fasta file

--- accession/human_metagenome/temp.py
+++ accession/human_metagenome/temp.py
@@ -1,19 +1,21 @@
 import csv
 
 # Open the bacteria_files.csv file
-with open('accession\human_metagenome\\bacteria_files.csv', 'r') as csvfile:
+with open('accession\human_metagenome\\archaea_files1.csv', 'r') as csvfile:
     reader = csv.reader(csvfile)
     rows = list(reader)
+    print(rows)
 
 # Read the content from temp.txt and store it in an array
 with open('accession\human_metagenome\\temp.txt', 'r') as file:
     content = file.readlines()
     array = [line.strip() for line in content]
+    print(array)
 
 # Delete rows where the first column value is in the array
 rows = [row for row in rows if row[0] not in array]
 
 # Write the updated rows back to the bacteria_files.csv file
-with open('bacteria_files_new.csv', 'w', newline='') as csvfile:
+with open('archaea_files.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerows(rows)
