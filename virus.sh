a8f366c4ba67650a5650a24e7ec9de23408c1ed7
Samadhi Kariyawasam <samadhi.19@cse.mrt.ac.lk>
2023-12-25 11:13:24 +0530

    added labels for the final fasta file

--- virus.sh
+++ virus.sh
@@ -1,12 +1,10 @@
-referance='/home/mbadmin/Documents/BashScripts/Helper/accession/human_metagenome/virus_files.csv'
+referance='/home/mbadmin/Documents/BashScripts/Helper/accession/shark_metagenome/virus_files.csv'
 
 
 while IFS=',' read -r -a line
 do 
     file_path=${line[1]}
     file_name=${line[0]}
-    dna_type=${line[2]}
-    no_of_reads=${line[3]}
-    ../../Simtools/NanoSim/src/simulator.py genome -rg $file_path -c ../../Simtools/NanoSim/pre-trained_models/metagenome_ERR3152364_Even/training -dna_type $dna_type -o human/reads/$file_name  -n  7
+    ../../../Simtools/NanoSim/src/simulator.py genome -rg $file_path -c ../../../Simtools/NanoSim/pre-trained_models/metagenome_ERR3152364_Even/training -dna_type linear -o reads/$file_name  -n 3
     
 done < "$referance"
