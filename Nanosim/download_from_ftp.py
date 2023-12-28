3a8b2f8d3ad819a34cc5db484c49ea30fe97800a
Samadhi Kariyawasam <samadhi.19@cse.mrt.ac.lk>
2023-12-28 23:19:49 +0530

    download new protozoa genome

--- Nanosim/download_from_ftp.py
+++ Nanosim/download_from_ftp.py
@@ -2,24 +2,20 @@ import os
 import csv
 import requests
 
-species = input("Enter species name: ")
+species = "Protozoa"
 assembly_summary_file = f"../../Genome/{species}/assembly_summary.txt"
-genome_dir = f"../../Genome/{species}/"
-accession_species_file = f"{species}.csv"
-accession_species = []
+genome_dir = f"../../Genome/{species}/test"
+downloaded_accession_file = "downloaded_protozoa.csv"
+downloaded_accessions = []
+accession_species_file = "test_protozoa_accession_and_speciesname.csv"
+downloaded_species = []
 
-# Read existing accession-species pairs from file
-if os.path.isfile(accession_species_file):
-	with open(accession_species_file, "r") as f:
+# read already downloaded accessions
+if os.path.isfile(downloaded_accession_file):
+	with open(downloaded_accession_file, "r") as f:
 		reader = csv.reader(f)
 		for row in reader:
-			accession_species.append(row[1])
-
-# Add header to file if it's empty
-if os.stat(accession_species_file).st_size == 0:
-	with open(accession_species_file, "w") as f:
-		writer = csv.writer(f)
-		writer.writerow(["accession", "species"])
+			downloaded_accessions.append(row[0])
 
 # Download genome files for new species
 with open(assembly_summary_file, "r") as f:
@@ -28,17 +24,19 @@ with open(assembly_summary_file, "r") as f:
 			continue
 		fields = line.strip().split("\t")
 		species_name=fields[7]
-		if species_name not in accession_species:
+		if species_name not in downloaded_species:
 			accession = fields[0]
-			url = fields[19]
-			filename = url.split("/")[-1] + "_genomic.fna.gz"
-			print(url,)
-			link = f"{url}/{filename}"
-			accession_species.append(species_name)
-			print(link)
-			response = requests.get(link)
-			with open(os.path.join(genome_dir, filename), "wb") as f:
-				f.write(response.content)
-			with open(accession_species_file, "a") as f:
-				writer = csv.writer(f)
-				writer.writerow([accession, species_name])
+			if accession not in downloaded_accessions:
+				
+				url = fields[19]
+				filename = url.split("/")[-1] + "_genomic.fna.gz"
+				print(url)
+				link = f"{url}/{filename}"
+				downloaded_species.append(species_name)
+				print(link)
+				response = requests.get(link)
+				with open(os.path.join(genome_dir, filename), "wb") as f:
+					f.write(response.content)
+				with open(accession_species_file, "a") as f:
+					writer = csv.writer(f)
+					writer.writerow([accession, species_name])
