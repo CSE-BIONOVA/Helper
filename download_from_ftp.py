faa18605d2fe22b5fb774f09b96877a034378303
Samadhi Kariyawasam <samadhi.19@cse.mrt.ac.lk>
2023-12-31 22:17:10 +0530

    download new protozoa genome

--- download_from_ftp.py
+++ download_from_ftp.py
@@ -2,12 +2,12 @@ import os
 import csv
 import requests
 
-species = "Fungi"
+species = "Archaea"
 assembly_summary_file = f"../../Genome/{species}/assembly_summary.txt"
 genome_dir = f"../../Genome/{species}/test"
-downloaded_accession_file = "downloaded_fungi.csv"
+downloaded_accession_file = "downloaded_archaea.csv"
 downloaded_accessions = []
-accession_species_file = "test_fungi_accession_and_speciesname.csv"
+accession_species_file = "test_archaea_accession_and_speciesname.csv"
 downloaded_species = []
 
 # read already downloaded accessions
