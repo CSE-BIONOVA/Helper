2ee668cc2801550e867423c8c52eaa808d4be1fa
Samadhi Kariyawasam <samadhi.19@cse.mrt.ac.lk>
2024-01-01 02:00:35 +0530

    generate protozoa reads

---
+++ generate_protozoa.sh
@@ -0,0 +1,9 @@
+referance='/home/mbadmin/Documents/BashScripts/Helper/accession/shark_metagenome/test/protozoa_files.csv'
+
+while IFS=',' read -r -a line
+do 
+    file_name=${line[0]}
+    file_path=${line[1]}
+    ../../../Simtools/NanoSim/src/simulator.py genome -rg $file_path -c ../../../Simtools/NanoSim/pre-trained_models/metagenome_ERR3152364_Even/training -dna_type linear -o reads/$file_name  -n 19
+    
+done < "$referance"
