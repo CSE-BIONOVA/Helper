3bae8f0b0192f095a2d8f03f341702d21ff3ead0
Samadhi Kariyawasam <samadhi.19@cse.mrt.ac.lk>
2024-01-08 02:00:37 +0530

    added more bacteria to the trainig set

---
+++ download_from_datasets_v.sh
@@ -0,0 +1,16 @@
+
+file="/home/mbadmin/Documents/BashScripts/Helper/viruses _human_vertabrate.csv"
+count=0
+
+while IFS= read -r line
+do
+  echo "Processing line: $line"
+  datasets download genome accession $line
+  unzip -o ncbi_dataset.zip 
+  touch $line.fna
+  mv ncbi_dataset/data/$line/*_genomic.fna $line.fna
+  count=$((count+1))
+  if [ $count -eq 7600 ]; then
+    break
+  fi
+done < "$file"
