70468ff831df86f5f2ce779e2ca3939a53a2ebd2
Samadhi Kariyawasam <samadhi.19@cse.mrt.ac.lk>
2024-01-13 11:42:22 +0530

    download all protozoa

--- download_from_datasets.sh
+++ download_from_datasets.sh
@@ -1,6 +1,5 @@
 
 file="/home/mbadmin/Documents/BashScripts/Helper/all_fungi.csv"
-count=0
 
 while IFS= read -r line
 do
@@ -9,8 +8,4 @@ do
   unzip -o ncbi_dataset.zip 
   touch $line.fna
   mv ncbi_dataset/data/$line/*_genomic.fna $line.fna
-  count=$((count+1))
-  if [ $count -eq 4600 ]; then
-    break
-  fi
 done < "$file"
