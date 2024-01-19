28c2068c3df63f84da265548fdb614e3f5560172
Samadhi Kariyawasam <samadhi.19@cse.mrt.ac.lk>
2024-01-19 22:11:01 +0530

    generate archaea reads

--- generate_reads/generate_archaea.sh
+++ generate_reads/generate_archaea.sh
@@ -4,6 +4,6 @@ referance='/home/mbadmin/Documents/BashScripts/Helper/genome_filenames/archaea.c
 while IFS=',' read -r -a line
 do 
     file_name=$(cut -c 1-15 <<< $line)
-    ../../Simtools/NanoSim/src/simulator.py genome -rg $line -c ../../Simtools/NanoSim/pre-trained_models/metagenome_ERR3152364_Even/training -dna_type linear -o reads/$file_name  -n 5 -med 6000
+    ../../Simtools/NanoSim/src/simulator.py genome -rg $line -c ../../Simtools/NanoSim/pre-trained_models/metagenome_ERR3152364_Even/training -dna_type linear -o reads/$file_name  -n 5 
     
 done < "$referance"
