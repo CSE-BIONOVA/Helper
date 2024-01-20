bbba7d5936d019970b62f94946dc5cecf8a44220
Samadhi Kariyawasam <samadhi.19@cse.mrt.ac.lk>
2024-01-20 10:38:13 +0530

    generate fungi

--- generate_reads/generate_fungi.sh
+++ generate_reads/generate_fungi.sh
@@ -1,9 +1,8 @@
-referance='/home/mbadmin/Documents/BashScripts/Helper/accession/shark_metagenome/test/fungi_files.csv'
+referance='/home/mbadmin/Documents/BashScripts/Helper/genome_filenames/fungi.csv'
 
 while IFS=',' read -r -a line
 do 
-    file_name=${line[0]}
-    file_path=${line[1]}
-    ../../../Simtools/NanoSim/src/simulator.py genome -rg $file_path -c ../../../Simtools/NanoSim/pre-trained_models/metagenome_ERR3152364_Even/training -dna_type linear -o reads/$file_name  -n 14
+    file_name=$(cut -c 1-15 <<< $line)
+    ../../Simtools/NanoSim/src/simulator.py genome -rg $line -c ../../Simtools/NanoSim/pre-trained_models/metagenome_ERR3152364_Even/training -dna_type linear -o reads/$file_name  -n 6
     
 done < "$referance"
