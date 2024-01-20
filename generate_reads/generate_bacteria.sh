23277698548902231dca157a7c161de255db4813
Samadhi Kariyawasam <samadhi.19@cse.mrt.ac.lk>
2024-01-21 01:09:22 +0530

    generate bacteria reads

--- generate_reads/generate_bacteria.sh
+++ generate_reads/generate_bacteria.sh
@@ -1,9 +1,9 @@
-referance='/home/mbadmin/Documents/BashScripts/Helper/accession/shark_metagenome/test/bacteria_files.csv'
+referance='/home/mbadmin/Documents/BashScripts/Helper/genome_filenames/temp_bacteria.csv'
 
 while IFS=',' read -r -a line
 do 
     file_name=${line[0]}
     file_path=${line[1]}
-    ../../../../Simtools/NanoSim/src/simulator.py genome -rg $file_path -c ../../../../Simtools/NanoSim/pre-trained_models/metagenome_ERR3152364_Even/training -dna_type linear -o reads/$file_name  -n 9
+    ../../Simtools/NanoSim/src/simulator.py genome -rg $file_path -c ../../Simtools/NanoSim/pre-trained_models/metagenome_ERR3152364_Even/training -dna_type linear -o reads/$file_name  -n 2
     
 done < "$referance"
