f6ff20dd1cd6db7e5e8015db4434c4aa5392dbb8
Samadhi Kariyawasam <samadhi.19@cse.mrt.ac.lk>
2024-02-08 11:11:33 +0530

    labels change

---
+++ misc/change_labels.sh
@@ -0,0 +1,8 @@
+cp ../../BashScripts/Helper/datasets/final/human_train_labels.csv human_train
+cp ../../BashScripts/Helper/datasets/final/human_test_labels.csv human_test
+cp ../../BashScripts/Helper/datasets/final/shark_test_1_labels.csv shark_test/shark_test_1
+cp ../../BashScripts/Helper/datasets/final/shark_test_25_labels.csv shark_test/shark_test_25
+cp ../../BashScripts/Helper/datasets/final/shark_test_50_labels.csv shark_test/shark_test_50
+cp ../../BashScripts/Helper/datasets/final/shark_test_75_labels.csv shark_test/shark_test_75
+cp ../../BashScripts/Helper/datasets/final/shark_test_99_labels.csv shark_test/shark_test_99
+cp ../../BashScripts/Helper/datasets/final/shark_train_labels.csv shark_train
