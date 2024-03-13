4db57d2cf23c67d8f065ab0bfaf7bb169ccdf75e
Samadhi Kariyawasam <samadhi.19@cse.mrt.ac.lk>
2024-03-13 13:50:23 +0530

    added simlord simulation script

--- generate_reads/simlord.sh
+++ generate_reads/simlord.sh
@@ -1,12 +1,15 @@
-coverage_list='coverage_list.csv'
-abundance_list='../../BashScripts/Helper/accession/human_metagenome/temp_files/abundance.csv'
-
-while IFS=',' read -r -a line <&3 && IFS=',' read -r -a line2 <&4
-do 
-    file_path=${line[0]}
-    coverage=${line[1]}
-    file_name=${line2[0]}
-    simlord --read-reference $file_path --coverage $coverage --no-sam $file_name 
-
-done 3< "$coverage_list" 4< "$abundance_list"
+csv_file="/home/mbadmin/Documents/BashScripts/Helper/all_reads/simlord/${input}_reads.csv"
+genome_files="/home/mbadmin/Documents/BashScripts/Helper/genome_filenames/og_${input}"
+while IFS='.' read -r -a line
+do
+    file_name="/home/mbadmin/Documents/${input^}/${line}"
+    genome_accession=${line[0]}
+    simlord --read-reference "$file_name" -fl 15000 --c 1 --no-sam "sim-reads/${genome_accession}"
+    no_of_lines=$(wc -l < "${genome_accession}.fastq")
+    no_of_reads=$((no_of_lines / 4))
+    seqkit fq2fa "${genome_accession}.fastq" -o "${genome_accession}.fasta"
+    rm "${genome_accession}.fastq"
+    echo "${genome_accession}.fasta,${no_of_reads}" >> "$csv_file"
+    
+done < "$genome_files"
 
